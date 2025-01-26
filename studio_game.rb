751fe2996173858ffeafd2fd0d806bbc5d153804
Alex <alexvdberg010109@gmail.com>
2025-01-26 16:31:00 +0200

    feat(): Crafting Classes

--- studio_game.rb
+++ studio_game.rb
@@ -2,12 +2,48 @@ player_name = "finn"
 player_health = 60
 
 
-def say_hello(name, health = 100)
-    "#{name.capitalize}\'s health is #{health}"
+class Player
+  def initialize(name, health = 100)
+    @name = name
+    @health = health
+  end
+
+  def to_s
+    "I am #{@name.capitalize} with a health of #{@health}"
+  end
+
+  def boost
+    @health += 15
+  end
+
+  def drain
+    @health -= 10
+  end
 end
 
+
 puts "Let's play a game!\n3\n2\n1"
-puts say_hello("finn", 60)
-puts say_hello("lucy", 90)
-puts say_hello("jase")
-puts say_hello("alex", 125)
+
+player_1 = Player.new("finn", 60)
+puts player_1
+player_1.boost
+puts player_1
+
+player_2 = Player.new("lucy", 90)
+puts player_2
+player_2.drain
+puts player_2
+
+player_3 = Player.new("jase")
+puts player_3
+player_3.boost
+player_3.boost
+puts player_3
+
+player_4 = Player.new("alex", 125)
+puts player_4.name
+player_4.name = "Alexander"
+puts player_4.name
+puts player_4.health
+puts player_4.score
+puts player_4
