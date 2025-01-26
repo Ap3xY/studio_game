b0b7d9637f5825a418103e9aecc7a46706988915
Alex <alexvdberg010109@gmail.com>
2025-01-26 15:08:56 +0200

    feat(): ABCs and 123s

--- studio_game.rb
+++ studio_game.rb
@@ -1,2 +1,5 @@
-puts "Lets play a game"
-puts "😂" * 25
+player_name = "Finn"
+player_health = 60
+
+puts "Let's play a game!\n\n\t3\n\t2\n\t1"
+puts "\n#{player_name}\'s health is #{player_health / 9.0}"
