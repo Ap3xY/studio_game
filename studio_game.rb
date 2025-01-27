37d5c809dbef2125b98ba848e3945ad7725a106b
Alex <alexvdberg010109@gmail.com>
2025-01-27 09:09:17 +0200

    feat(): Objects Collaborating

--- studio_game.rb
+++ studio_game.rb
@@ -21,35 +21,56 @@ class Player
     end
 end
 
+class Game
+  def initialize(name)
+    @name = name
+    @players = []
+  end
 
-puts "Let's play a game!\n3\n2\n1\n\n"
+  def add_player(player)
+    @players.push(player)
+  end
+
+  def roll_die
+    rand(1..6)
+  end
+
+  def play
+    puts "Let's play a game!\n3\n2\n1\n\n"
+
+    @players.each do | player |
+        case roll_die
+        when 1..2
+            player.drain
+            puts "#{player.name} got drained 😩"
+        when 3..4
+            puts "#{player.name} got skipped"
+        else
+            player.boost
+            puts "#{player.name} got boosted 😁"
+        end
+    end
+
+    puts "\nAfter playing:"
+    puts @players
+  end
+end
 
 player_1 = Player.new("Alex", 60)
 player_2 = Player.new("Sheldon", 90)
 player_3 = Player.new("Sean")
 player_4 = Player.new("Monique", 125)
-player_5 = Player.new("Luan", 10)
+game = Game.new("Winner Takes All")
 
-players = [player_1, player_2, player_3, player_4, player_5]
+game.add_player(player_1)
+game.add_player(player_2)
+game.add_player(player_3)
+game.add_player(player_4)
+game.play
 
 
-players.each do | player |
 
-    number_rolled = rand(1..6)
-    case number_rolled
-    when 1..2
-        player_1.drain
-        puts "#{player.name} got drained 😩"
-    when 3..4
-        puts "#{player.name} got skipped"
-    else
-        player_1.boost
-        puts "#{player.name} got boosted 😁"
-    end
-end
 
-puts "\nAfter playing:"
-puts players
 
 
 
