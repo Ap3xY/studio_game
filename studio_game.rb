8053aa1a156aaa49ea0ebb89a4701f212988a75e
Alex <alexvdberg010109@gmail.com>
2025-02-07 09:29:10 +0200

    feat(): File Ins and Outs

--- studio_game.rb
+++ studio_game.rb
@@ -1,16 +1,10 @@
 require_relative "./lib/game"
 require_relative "./lib/player"
 
-player_1 = Player.new("Alex", 60)
-player_2 = Player.new("Sheldon", 90)
-player_3 = Player.new("Sean")
-player_4 = Player.new("Monique", 125)
 game = Game.new("Winner Takes All")
 
-game.add_player(player_1)
-game.add_player(player_2)
-game.add_player(player_3)
-game.add_player(player_4)
+players_file = File.join(__dir__, "players.csv")
+game.load_players(ARGV.shift || players_file)
 
 loop do
   print "\nHow many game rounds? ('quit' to exit) "
@@ -26,3 +20,5 @@ loop do
     puts "Please enter a number or 'quit'"
   end
 end
+
+game.save_high_scores
